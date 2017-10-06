e0a6f92f5b7da80e6ff013d2038b2f165f4ceaef
Jim Mainprice <mainprice@gmail.com>
2017-10-06 13:13:28 +0200

    Add clone script

---
+++ clone_human_repos.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/jmainpri/human_bio_scene.git
+git clone https://github.com/jmainpri/human_bio_urdf.git
+git clone https://github.com/jmainpri/human_bio_dataset.git
+git clone https://github.com/jmainpri/human_bio_octomap.git
