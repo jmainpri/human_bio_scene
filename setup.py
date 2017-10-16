1f62d5e9451d40feb73a1d828d618f93937f16fe
Jim Mainprice <mainprice@gmail.com>
2017-10-16 14:42:39 +0200

    fix setup dot py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from catkin_pkg import python_setup
 
 # fetch values from package.xml
 setup_args = python_setup.generate_distutils_setup(
-    packages=['.'],
+    packages=['human_bio_scene'],
     package_dir={'': 'src'})
 
 setup(**setup_args)
