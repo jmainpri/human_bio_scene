d3bdf637fadcd1c91b674dd246a96751aade34c3
Jim Mainprice <mainprice@gmail.com>
2020-05-04 18:36:45 +0200

    Update clone_human_repos.sh

--- clone_human_repos.sh
+++ clone_human_repos.sh
@@ -1,4 +1,7 @@
-git clone https://github.com/jmainpri/human_bio_scene.git
-git clone https://github.com/jmainpri/human_bio_urdf.git
-git clone https://github.com/jmainpri/human_bio_dataset.git
-git clone https://github.com/jmainpri/human_bio_octomap.git
+GITHUB=https://github.com/jmainpri
+BRANCH=bionic-beaver-melodic-dev
+git clone $GITHUB/human_bio_octomap.git
+git clone $GITHUB/human_bio_urdf.git
+git clone $GITHUB/human_bio_scene.git
+git clone $GITHUB/human_bio_dataset.git
+cd human_bio_octomap && git checkout $BRANCH && cd ..
